891e154311c00d15b815348200efa3355d6ef0ae
Barry <870709864@qq.com>
2022-10-31 23:00:59 +0800

    feat: mojo基础用法

--- src/akama-sdk/sample/ipc/main.cc
+++ src/akama-sdk/sample/ipc/main.cc
@@ -15,6 +15,42 @@
 // mojo cpp system api,使用较多：https://chromium.googlesource.com/chromium/src/+/refs/tags/103.0.5060.126/mojo/public/cpp/system/README.md
 // mojo c system api,使用较少：https://chromium.googlesource.com/chromium/src/+/refs/tags/103.0.5060.126/mojo/public/c/system/README.md
 // clang-format on
+
+class LoggerImpl : public sample::mojom::Logger {
+ public:
+  // NOTE: A common pattern for interface implementations which have one
+  // instance per client is to take a PendingReceiver in the constructor.
+  // Receiver通过PendingReceiver从pipe接收消息，并反序列化消息以后调用T的方法。
+  explicit LoggerImpl(
+      mojo::PendingReceiver<sample::mojom::Logger> pending_receiver)
+      : receiver_(this, std::move(pending_receiver)) {
+    receiver_.set_disconnect_handler(
+        base::BindOnce(&LoggerImpl::OnError, base::Unretained(this)));
+  }
+  LoggerImpl(const LoggerImpl&) = delete;
+  LoggerImpl& operator=(const LoggerImpl&) = delete;
+  ~LoggerImpl() override {}
+
+  // sample::mojom::Logger:
+  void Log(const std::string& message) override {
+    std::cout << "[Logger] " << message << std::endl;
+    lines_.push_back(message);
+  }
+
+  void GetTail(GetTailCallback callback) override {
+    std::move(callback).Run(lines_.back());
+  }
+
+  void OnError() {
+    std::cout << "Client disconnected! Purging log lines." << std::endl;
+    lines_.clear();
+  }
+
+ private:
+  mojo::Receiver<sample::mojom::Logger> receiver_;
+  std::vector<std::string> lines_;
+};
+
 int main(int argc, char* argv[]) {
   std::cout << "start ipc:" << base::Time::Now() << std::endl;
 
@@ -31,16 +67,38 @@ int main(int argc, char* argv[]) {
 
   ipc_thread.task_runner()->PostTask(
       FROM_HERE, base::BindOnce([]() {
-        /*
-        mojo::MessagePipe pipe;
-        mojo::Remote<sample::mojom::Logger> logger(
-            mojo::PendingRemote<sample::mojom::Logger>(std::move(pipe.handle0),
-        0)); mojo::PendingReceiver<sample::mojom::Logger> receiver(
-            std::move(pipe.handle1));
-            */
-        mojo::Remote<sample::mojom::Logger> logger;
-        auto receiver = logger.BindNewPipeAndPassReceiver();
+  // 两种方式等效：本质就是构造一个MessagePipe，sender和receiver分别绑定到两端来发送和接收
+  // PendingRemote是发送端对pipe的封装，PendingReceiver是接收端对pipe的封装
+  // Remote负责序列化T的接口调用，并通过PendingRemote把消息发送到pipe
+  // Receiver通过PendingReceiver从pipe接收消息，反序列化消息以后调用T的接口
+  //
+  // mojo cpp binding api
+  // 使进程间通信像创建一个接口对象（Logger接口指针指向LoggerImpl实现）并调用其方法一样简单。
+  // Remote持有对象的接口，Receiver持有对象实现
+#if 0
+            mojo::MessagePipe pipe;
+            auto* sender = new mojo::Remote<sample::mojom::Logger>(
+                mojo::PendingRemote<sample::mojom::Logger>(
+                    std::move(pipe.handle0), 0));
+            mojo::PendingReceiver<sample::mojom::Logger> pending_receiver(
+                std::move(pipe.handle1));
+#else
+        mojo::Remote<sample::mojom::Logger>* sender =
+            new mojo::Remote<sample::mojom::Logger>;
+        auto pending_receiver = sender->BindNewPipeAndPassReceiver();
+#endif
+        LoggerImpl* recver = new LoggerImpl(std::move(pending_receiver));
+        (void)recver;
+
+        (*sender)->Log("Hello!");
+        (*sender)->GetTail(base::BindOnce([](const std::string& tail) {
+          std::cout << "tail: " << tail << std::endl;
+        }));
+
+        // 模拟断开管道，触发LoggerImpl::OnError
+        // sender->reset();
       }));
+  // base::PlatformThread::Sleep(base::Milliseconds(1000));
   ipc_thread.Stop();
 
   MojoShutdown(nullptr);
